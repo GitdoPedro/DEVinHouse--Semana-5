5623fcb033305c69f8feda5dd2a0b2a45893ca26
Pedro Lima <emailpedrolima@gmail.com>
2022-11-01 16:51:38 -0300

    primeiro exercicio

---
+++ ex01.js
@@ -0,0 +1,18 @@
+/*Em um arquivo app.js incluído em index.html de tipo módulo type="module":
+
+Crie uma classe Funcionario para representar um(a) empregado(a) de uma empresa, contendo os atributos cpf, nomeCompleto e salario.
+
+Implemente também um método promover(percentual) que recebe como parâmetro um percentual, devendo o salário ser aumentado pelo percentual indicado no argumento do método.
+
+Crie e utilize um objeto desta classe para testar a implementação.
+
+Exemplo:
+
+const ada = new Funcionario("528.442.040-31", "Ada Lovelace", 1000);
+
+console.log(ada.salario); // 1000
+
+ada.promover(50);
+
+console.log(ada.salario); // 1500*/
+
